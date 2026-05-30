7e0ab91853faa62d85e8817ca7e36c1e9b12fde1
zhanghaoyang76 <15811505615@163.com>
2026-05-30 14:59:50 +0800

    V1.0

---
+++ README.md
@@ -0,0 +1,109 @@
+<picture>
+    <source srcset="./.github/logo-dark.png" media="(prefers-color-scheme: light)">
+    <source srcset="./.github/logo-white.png" media="(prefers-color-scheme: dark)">
+    <img src="./.github/logo-dark.png" alt="logo">
+</picture>
+
+
+
+## 自托管
+
+适用于家庭实验室的自托管方案。
+
+**通过 Docker Hub：**
+
+```sh
+docker run -d --name it-tools --restart unless-stopped -p 8080:80 corentinth/it-tools:latest
+```
+
+**通过 GitHub Packages：**
+
+```sh
+docker run -d --name it-tools --restart unless-stopped -p 8080:80 ghcr.io/corentinth/it-tools:latest
+```
+
+**其他方案：**
+
+- [Cloudron](https://www.cloudron.io/store/tech.ittools.cloudron.html)
+- [Tipi](https://www.runtipi.io/docs/apps-available)
+- [Unraid](https://unraid.net/community/apps?q=it-tools)
+
+## 贡献
+
+### 推荐的 IDE 配置
+
+推荐使用 [VSCode](https://code.visualstudio.com/) 并安装以下扩展：
+
+- [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)（并禁用 Vetur）
+- [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin)
+- [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+- [i18n Ally](https://marketplace.visualstudio.com/items?itemName=lokalise.i18n-ally)
+
+建议使用以下设置：
+
+```json
+{
+  "editor.formatOnSave": false,
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  },
+  "i18n-ally.localesPaths": ["locales", "src/tools/*/locales"],
+  "i18n-ally.keystyle": "nested"
+}
+```
+
+### TypeScript 中 `.vue` 导入的类型支持
+
+TypeScript 默认无法处理 `.vue` 导入的类型信息，因此我们使用 `vue-tsc` 替代 `tsc` CLI 来进行类型检查。在编辑器中，需要安装 [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin)，让 TypeScript 语言服务能够识别 `.vue` 类型。
+
+如果你觉得独立的 TypeScript 插件速度不够快，Volar 还实现了性能更好的 [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669)。可以按以下步骤启用：
+
+1. 禁用内置的 TypeScript 扩展
+   1. 在 VSCode 命令面板中运行 `Extensions: Show Built-in Extensions`
+   2. 找到 `TypeScript and JavaScript Language Features`，右键并选择 `Disable (Workspace)`
+2. 在命令面板中运行 `Developer: Reload Window`，重新加载 VSCode 窗口。
+
+### 项目初始化
+
+```sh
+pnpm install
+```
+
+### 开发环境编译与热重载
+
+```sh
+pnpm dev
+```
+
+### 生产环境类型检查、编译与压缩
+
+```sh
+pnpm build
+```
+
+### 使用 [Vitest](https://vitest.dev/) 运行单元测试
+
+```sh
+pnpm test
+```
+
+### 使用 [ESLint](https://eslint.org/) 进行代码检查
+
+```sh
+pnpm lint
+```
+
+### 创建新工具
+
+如需创建新工具，可以运行脚本来生成新工具的样板代码：
+
+```sh
+pnpm run script:create:tool my-tool-name
+```
+
+它会在 `src/tools` 中创建包含正确文件的目录，并在 `src/tools/index.ts` 中添加导入。你只需要把导入的工具添加到合适的分类中，然后开发该工具即可。
+
+
+## 声明
+
+本项目来自https://github.com/CorentinTh/it-tools，本人作为开发小白，抱着学习态度，想把项目复现以及增加新功能在自己的站点，感谢原作者的分享
---
+++ src/pages/Home.page.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+import { IconDragDrop, IconHeart } from '@tabler/icons-vue';
+import { useHead } from '@vueuse/head';
+import { computed } from 'vue';
+import Draggable from 'vuedraggable';
+import ColoredCard from '../components/ColoredCard.vue';
+import ToolCard from '../components/ToolCard.vue';
+import { useToolStore } from '@/tools/tools.store';
+import { config } from '@/config';
+
+const toolStore = useToolStore();
+
+useHead({ title: 'IT Tools - Handy online tools for developers' });
+const { t } = useI18n();
+
+const favoriteTools = computed(() => toolStore.favoriteTools);
+
+// Update favorite tools order when drag is finished
+function onUpdateFavoriteTools() {
+  toolStore.updateFavoriteTools(favoriteTools.value); // Update the store with the new order
+}
+</script>
+
+<template>
+  <div class="pt-50px">
+    <div class="grid-wrapper">
+      <div class="grid grid-cols-1 gap-12px lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4">
+        <ColoredCard v-if="config.showBanner" :title="$t('home.follow.title')" :icon="IconHeart">
+          {{ $t('home.follow.p1') }}
+          <a
+            href="https://github.com/CorentinTh/it-tools"
+            rel="noopener"
+            target="_blank"
+            :aria-label="$t('home.follow.githubRepository')"
+          >GitHub</a>
+          {{ $t('home.follow.p2') }}
+          <a
+            href="https://x.com/ittoolsdottech"
+            rel="noopener"
+            target="_blank"
+            :aria-label="$t('home.follow.twitterXAccount')"
+          >X</a>.
+          {{ $t('home.follow.thankYou') }}
+          <n-icon :component="IconHeart" />
+        </ColoredCard>
+      </div>
+
+      <transition name="height">
+        <div v-if="toolStore.favoriteTools.length > 0">
+          <h3 class="mb-5px mt-25px text-neutral-400 font-500">
+            {{ $t('home.categories.favoriteTools') }}
+            <c-tooltip :tooltip="$t('home.categories.favoritesDndToolTip')">
+              <n-icon :component="IconDragDrop" size="18" />
+            </c-tooltip>
+          </h3>
+          <Draggable
+            :list="favoriteTools"
+            class="grid grid-cols-1 gap-12px lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4"
+            ghost-class="ghost-favorites-draggable"
+            item-key="name"
+            @end="onUpdateFavoriteTools"
+          >
+            <template #item="{ element: tool }">
+              <ToolCard :tool="tool" />
+            </template>
+          </Draggable>
+        </div>
+      </transition>
+
+      <div v-if="toolStore.newTools.length > 0">
+        <h3 class="mb-5px mt-25px text-neutral-400 font-500">
+          {{ t('home.categories.newestTools') }}
+        </h3>
+        <div class="grid grid-cols-1 gap-12px lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4">
+          <ToolCard v-for="tool in toolStore.newTools" :key="tool.name" :tool="tool" />
+        </div>
+      </div>
+
+      <h3 class="mb-5px mt-25px text-neutral-400 font-500">
+        {{ $t('home.categories.allTools') }}
+      </h3>
+      <div class="grid grid-cols-1 gap-12px lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4">
+        <ToolCard v-for="tool in toolStore.tools" :key="tool.name" :tool="tool" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.height-enter-active,
+.height-leave-active {
+  transition: all 0.5s ease-in-out;
+  overflow: hidden;
+  max-height: 500px;
+}
+
+.height-enter-from,
+.height-leave-to {
+  max-height: 42px;
+  overflow: hidden;
+  opacity: 0;
+  margin-bottom: 0;
+}
+
+.ghost-favorites-draggable {
+  opacity: 0.4;
+  background-color: #ccc;
+  border: 2px dashed #666;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  transform: scale(1.1);
+  animation: ghost-favorites-draggable-animation 0.2s ease-out;
+}
+
+@keyframes ghost-favorites-draggable-animation {
+  0% {
+    opacity: 0;
+    transform: scale(0.9);
+  }
+  100% {
+    opacity: 0.4;
+    transform: scale(1.0);
+  }
+}
+</style>
